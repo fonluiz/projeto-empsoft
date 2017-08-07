749ef94b30f9d64323921eeed64bb472dad56d45
carolwanderley <ana.caroline.silva@ccc.ufcg.edu.br>
2017-08-07 11:52:38 -0300

    feed

--- MelhorIdade/app/src/main/java/com/example/melhoridade/melhoridade/SelectHobbiesActivity.java
+++ MelhorIdade/app/src/main/java/com/example/melhoridade/melhoridade/SelectHobbiesActivity.java
@@ -1,14 +1,19 @@
 package com.example.melhoridade.melhoridade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectHobbiesActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private Button xablau;
     private String[] hobbiesDataset = new String[] {
             "Culinária", "Novelas", "Esportes", "Viagens", "Namoro", "Piadas"
     };
@@ -20,6 +25,14 @@ public class SelectHobbiesActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.hobbies_recycler_view);
 
+        xablau = (Button) findViewById(R.id.next);
+        xablau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SelectHobbiesActivity.this, FeedActivity.class);
+                startActivity(i);
+            }
+        });
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
